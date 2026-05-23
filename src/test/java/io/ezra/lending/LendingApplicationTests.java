4babaefcb85e0214298b8ae13b5ff5c3882e1502
Harrison M. Mungai <muhari.harrison@eclectics.io>
2026-05-23 13:11:38 +0300

    Initial commit

---
+++ src/test/java/io/ezra/lending/LendingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ezra.lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
